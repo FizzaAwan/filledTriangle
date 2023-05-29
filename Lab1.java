248cb58a2bfb9c11f8a93de13bcddc062bc3fde8
Phys <112678793+FizzaAwan@users.noreply.github.com>
2023-05-29 12:34:41 -0400

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,14 @@
+/** 
+   A program to draw a filled triangle.
+*/
+
+public class Lab1
+{
+   public static void main(String[] args)
+   {
+      System.out.println("   x");
+
+      /* Your work here */
+
+   }
+}
